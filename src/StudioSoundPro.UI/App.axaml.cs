543070e1a7b7b65f7f7e60049476cc295223bc36
Reyis Jones <reyisj@gmail.com>
2025-11-05 22:14:23 -0400

    Update application name to 'Studio Sound Pro' in UI and macOS menu bar
    - Changed window title from 'StudioSoundPro' to 'Studio Sound Pro'
    - Added CFBundleDisplayName and CFBundleName properties to csproj
    - Set Application.Name property in App.axaml.cs
    - Created custom NativeMenu for macOS to replace default Avalonia menu
    - Menu bar now displays 'Studio Sound Pro' instead of 'Avalonia Application'

--- src/StudioSoundPro.UI/App.axaml.cs
+++ src/StudioSoundPro.UI/App.axaml.cs
@@ -4,6 +4,8 @@ using Avalonia.Data.Core;
 using Avalonia.Data.Core.Plugins;
 using System.Linq;
 using Avalonia.Markup.Xaml;
+using Avalonia.Controls;
+using Avalonia.Input;
 using StudioSoundPro.UI.ViewModels;
 using StudioSoundPro.UI.Views;
 
@@ -24,7 +26,28 @@ public partial class App : Application
             // More info: https://docs.avaloniaui.net/docs/guides/development-guides/data-validation#manage-validationplugins
             DisableAvaloniaDataAnnotationValidation();
             
+            // Set application name BEFORE creating the window
+            Name = "Studio Sound Pro";
+            
             desktop.MainWindow = new MainWindow();
+            
+            // Create native menu for macOS - this replaces the default Avalonia menu
+            var menu = new NativeMenu();
+            var appMenu = new NativeMenuItem();
+            var appSubMenu = new NativeMenu();
+            
+            appSubMenu.Add(new NativeMenuItem("About Studio Sound Pro"));
+            appSubMenu.Add(new NativeMenuItemSeparator());
+            appSubMenu.Add(new NativeMenuItem("Hide Studio Sound Pro") { Gesture = KeyGesture.Parse("CMD+H") });
+            appSubMenu.Add(new NativeMenuItem("Hide Others") { Gesture = KeyGesture.Parse("CMD+ALT+H") });
+            appSubMenu.Add(new NativeMenuItem("Show All"));
+            appSubMenu.Add(new NativeMenuItemSeparator());
+            appSubMenu.Add(new NativeMenuItem("Quit Studio Sound Pro") { Gesture = KeyGesture.Parse("CMD+Q") });
+            
+            appMenu.Menu = appSubMenu;
+            menu.Add(appMenu);
+            
+            NativeMenu.SetMenu(desktop.MainWindow, menu);
             // The AudioTestView will create its own DataContext via design-time support
         }
 
